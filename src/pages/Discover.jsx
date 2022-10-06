b133d62e7c8458df3aed5b9d928bb51f4980be02
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-06 15:22:33 +0530

    first commit

---
+++ src/pages/Discover.jsx
@@ -0,0 +1,33 @@
+import { Error, Loader, SongCard } from "../components";
+import { genres } from "../assets/constants";
+
+const Discover = () => {
+  const genreTitle = "Pop";
+  return (
+    <div className="flex flex-col">
+      <div className="w-full flex items-center justify-between sm:flex-row flex-col mt-4 mb-10">
+        <h2 className="font-bold text-white text-left text-3xl">
+          Discover {genreTitle}
+        </h2>
+        <select
+          onChange={() => {}}
+          value=""
+          className="bg-black text-gray-300 outline-none p-3 text-sm rounded-lg sm:mt-0 mt-5"
+        >
+            {
+                genres.map((genres) => <option key={genres.value} value={genres.value}>{genres.title}</option>)
+            }
+        </select>
+      </div>
+      <div className="flex flex-wrap justify-center gap-8 sm:justify-start">
+        {
+            [1,2,3,4,5,6,7,8].map((song, i) =>(
+                <SongCard key={song.key} song={song} i={i} />
+            ))
+        }
+      </div>
+    </div>
+  );
+};
+
+export default Discover;
