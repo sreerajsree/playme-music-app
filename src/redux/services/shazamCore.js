89b4a75f6eb4649a4079e69ad0b196d5d03505c0
sreerajsree <sreeandsreeraj@gmail.com>
2022-10-07 15:58:51 +0530

    data

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -1,23 +1,32 @@
-import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const shazamCoreApi = createApi({
-  reducerPath: "shazamCoreApi",
+  reducerPath: 'shazamCoreApi',
   baseQuery: fetchBaseQuery({
-    baseUrl: "https://shazam-core.p.rapidapi.com/v1",
+    baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
     prepareHeaders: (headers) => {
-      headers.set(
-        "X-RapidAPI-Key",
-        "fc618428f2msh0011d052f97b82bp1e148ajsnfb017a6e88e8"
-      );
+      headers.set('X-RapidAPI-Key', 'fc618428f2msh0011d052f97b82bp1e148ajsnfb017a6e88e8');
+
       return headers;
     },
   }),
-  endpoints : (builder) => ({
-    getTopCharts : builder.query({ query: () => '/charts/world' })
-  })
+  endpoints: (builder) => ({
+    getTopCharts: builder.query({ query: () => '/charts/world' }),
+    getSongsByGenre: builder.query({ query: (genre) => `/charts/genre-world?genre_code=${genre}` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
+    getSongsBySearch: builder.query({ query: (searchTerm) => `/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+    getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${artistId}` }),
+    getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${songid}` }),
+    getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${songid}` }),
+  }),
 });
 
-
 export const {
-    useGetTopChartsQuery,
+  useGetTopChartsQuery,
+  useGetSongsByGenreQuery,
+  useGetSongsByCountryQuery,
+  useGetSongsBySearchQuery,
+  useGetArtistDetailsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
 } = shazamCoreApi;
